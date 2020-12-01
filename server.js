d6147e1ecec2c5b3e1b5646e50478d233b79f857
Nicholas Yu <nyu@shaw.ca>
2020-11-30 22:50:25 -0800

    fixed

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ if(process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'));
 
-  // app.get('*', (req,res) => {
-  //   res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
-  // })
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  })
 }
 
 const port = process.env.PORT || 5000;
